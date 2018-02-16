662b5d41c07e8a197acbcd44a7b2a0e638e25aa0
James Risberg <james@verigo.io>
2018-02-15 17:51:05 -0800

    Formatted logs for comprehension, handle success cases

--- src/main/java/ExchangeTester.java
+++ src/main/java/ExchangeTester.java
@@ -53,8 +53,16 @@ public class ExchangeTester {
 
         CurrencyPair testPair = CurrencyPair.ETH_BTC;
 
+        System.out.println("******************  Market Data Service Tests!  ******************\n\n");
+
         try {
+            System.out.println("Trying liquiMarketDataService.getTicker(CurrencyPair.ETH_BTC)\n");
+
             Ticker ticker = liquiMarketDataService.getTicker(testPair, null, null);
+
+            System.out.println("Success! Result:");
+            System.out.println(ticker.toString());
+            System.out.println("\n\n----------------------------------------------------------------------\n\n");
         } catch (NotAvailableFromExchangeException e) {
             System.err.println("NotAvailableFromExchangeException " + e.getMessage());
         } catch (NotYetImplementedForExchangeException e) {
@@ -70,7 +78,13 @@ public class ExchangeTester {
         }
 
         try {
+            System.out.println("Trying liquiMarketDataService.getOrderBook(CurrencyPair.ETH_BTC)\n");
+
             OrderBook orderBook = liquiMarketDataService.getOrderBook(testPair, null, null);
+
+            System.out.println("Success! Result:");
+            System.out.println(orderBook.toString());
+            System.out.println("\n\n----------------------------------------------------------------------\n\n");
         } catch (NotAvailableFromExchangeException e) {
             System.err.println("NotAvailableFromExchangeException " + e.getMessage());
         } catch (NotYetImplementedForExchangeException e) {
@@ -86,7 +100,13 @@ public class ExchangeTester {
         }
 
         try {
+            System.out.println("Trying liquiMarketDataService.getTrades(CurrencyPair.ETH_BTC)\n");
+
             Trades trades = liquiMarketDataService.getTrades(testPair, null, null);
+
+            System.out.println("Success! Result:");
+            System.out.println(trades.toString());
+            System.out.println("\n\n----------------------------------------------------------------------\n\n");
         } catch (NotAvailableFromExchangeException e) {
             System.err.println("NotAvailableFromExchangeException " + e.getMessage());
         } catch (NotYetImplementedForExchangeException e) {
@@ -105,8 +125,16 @@ public class ExchangeTester {
     private static void testAccountService(final Exchange liquiExchange) throws IOException {
         final LiquiAccountService liquiAccountService = (LiquiAccountService) liquiExchange.getAccountService();
 
+        System.out.println("******************  Account Service Tests!  ******************\n\n");
+
         try {
+            System.out.println("Trying liquiAccountService.getAccountInfo()\n");
+
             AccountInfo accountInfo = liquiAccountService.getAccountInfo();
+
+            System.out.println("Success! Result:");
+            System.out.println(accountInfo.toString());
+            System.out.println("\n\n----------------------------------------------------------------------\n\n");
         } catch (NotAvailableFromExchangeException e) {
             System.err.println("NotAvailableFromExchangeException " + e.getMessage());
         } catch (NotYetImplementedForExchangeException e) {
@@ -122,10 +150,16 @@ public class ExchangeTester {
         }
 
         try {
+            System.out.println("Trying liquiAccountService.withdrawFunds(Currency.BTC, new BigDecimal(0.01), \"ADDRESS\")\n");
+
             String address = "ADDRESS";
             BigDecimal amount = new BigDecimal(0.01);
 
             String response = liquiAccountService.withdrawFunds(Currency.BTC, amount, address);
+
+            System.out.println("Success! Result:");
+            System.out.println(response);
+            System.out.println("\n\n----------------------------------------------------------------------\n\n");
         } catch (NotAvailableFromExchangeException e) {
             System.err.println("NotAvailableFromExchangeException " + e.getMessage());
         } catch (NotYetImplementedForExchangeException e) {
@@ -141,7 +175,13 @@ public class ExchangeTester {
         }
 
         try {
+            System.out.println("Trying liquiAccountService.requestDepositAddress(Currency.BTC)\n");
+
             String response = liquiAccountService.requestDepositAddress(Currency.BTC, null, null);
+
+            System.out.println("Success! Result:");
+            System.out.println(response);
+            System.out.println("\n\n----------------------------------------------------------------------\n\n");
         } catch (NotAvailableFromExchangeException e) {
             System.err.println("NotAvailableFromExchangeException " + e.getMessage());
         } catch (NotYetImplementedForExchangeException e) {
@@ -157,9 +197,15 @@ public class ExchangeTester {
         }
 
         try {
+            System.out.println("Trying liquiAccountService.getFundingHistory(tradeHistoryParams)\n");
+
             TradeHistoryParams params = liquiAccountService.createFundingHistoryParams();
 
             List<FundingRecord> records = liquiAccountService.getFundingHistory(params);
+
+            System.out.println("Success! Result:");
+            System.out.println(records.toArray().toString());
+            System.out.println("\n\n----------------------------------------------------------------------\n\n");
         } catch (NotAvailableFromExchangeException e) {
             System.err.println("NotAvailableFromExchangeException " + e.getMessage());
         } catch (NotYetImplementedForExchangeException e) {
@@ -178,8 +224,16 @@ public class ExchangeTester {
     private static void testTradeService(final Exchange liquiExchange) throws IOException {
         final LiquiTradeService liquiTradeService = (LiquiTradeService) liquiExchange.getTradeService();
 
+        System.out.println("******************  Trade Service Tests!  ******************\n\n");
+
         try {
+            System.out.println("Trying liquiTradeService.getOpenOrders()\n");
+
             OpenOrders openOrders = liquiTradeService.getOpenOrders();
+
+            System.out.println("Success! Result:");
+            System.out.println(openOrders.toString());
+            System.out.println("\n\n----------------------------------------------------------------------\n\n");
         } catch (NotAvailableFromExchangeException e) {
             System.err.println("NotAvailableFromExchangeException " + e.getMessage());
         } catch (NotYetImplementedForExchangeException e) {
@@ -195,8 +249,14 @@ public class ExchangeTester {
         }
 
         try {
+            System.out.println("Trying liquiTradeService.placeMarketOrder(marketOrder)\n");
+
             MarketOrder order = new MarketOrder(Order.OrderType.BID, new BigDecimal(0.01), CurrencyPair.BTC_USD);
             String response = liquiTradeService.placeMarketOrder(order);
+
+            System.out.println("Success! Result:");
+            System.out.println(response);
+            System.out.println("\n\n----------------------------------------------------------------------\n\n");
         } catch (NotAvailableFromExchangeException e) {
             System.err.println("NotAvailableFromExchangeException " + e.getMessage());
         } catch (NotYetImplementedForExchangeException e) {
@@ -212,8 +272,14 @@ public class ExchangeTester {
         }
 
         try {
+            System.out.println("Trying liquiTradeService.placeLimitOrder(limitOrder)\n");
+
             LimitOrder order = new LimitOrder(Order.OrderType.BID, new BigDecimal(0.01), CurrencyPair.BTC_USD, "ID", Date.from(Instant.now()), new BigDecimal(10000));
             String response = liquiTradeService.placeLimitOrder(order);
+
+            System.out.println("Success! Result:");
+            System.out.println(response);
+            System.out.println("\n\n----------------------------------------------------------------------\n\n");
         } catch (NotAvailableFromExchangeException e) {
             System.err.println("NotAvailableFromExchangeException " + e.getMessage());
         } catch (NotYetImplementedForExchangeException e) {
@@ -229,8 +295,14 @@ public class ExchangeTester {
         }
 
         try {
+            System.out.println("Trying liquiTradeService.placeStopOrder(stopOrder)\n");
+
             StopOrder order = new StopOrder(Order.OrderType.BID, new BigDecimal(0.01), CurrencyPair.BTC_USD, "ID", Date.from(Instant.now()), new BigDecimal(10000));
             String response = liquiTradeService.placeStopOrder(order);
+
+            System.out.println("Success! Result:");
+            System.out.println(response);
+            System.out.println("\n\n----------------------------------------------------------------------\n\n");
         } catch (NotAvailableFromExchangeException e) {
             System.err.println("NotAvailableFromExchangeException " + e.getMessage());
         } catch (NotYetImplementedForExchangeException e) {
@@ -246,7 +318,13 @@ public class ExchangeTester {
         }
 
         try {
+            System.out.println("Trying liquiTradeService.cancelOrder(orderID)\n");
+
             boolean canceled = liquiTradeService.cancelOrder("1");
+
+            System.out.println("Success! Result:");
+            System.out.println(canceled);
+            System.out.println("\n\n----------------------------------------------------------------------\n\n");
         } catch (NotAvailableFromExchangeException e) {
             System.err.println("NotAvailableFromExchangeException " + e.getMessage());
         } catch (NotYetImplementedForExchangeException e) {
@@ -262,8 +340,14 @@ public class ExchangeTester {
         }
 
         try {
+            System.out.println("Trying liquiTradeService.getTradeHistory(tradeHistoryParams)\n");
+
             TradeHistoryParams params = liquiTradeService.createTradeHistoryParams();
             UserTrades trades = liquiTradeService.getTradeHistory(params);
+
+            System.out.println("Success! Result:");
+            System.out.println(trades.getUserTrades().toArray().toString());
+            System.out.println("\n\n----------------------------------------------------------------------\n\n");
         } catch (NotAvailableFromExchangeException e) {
             System.err.println("NotAvailableFromExchangeException " + e.getMessage());
         } catch (NotYetImplementedForExchangeException e) {
@@ -279,7 +363,13 @@ public class ExchangeTester {
         }
 
         try {
+            System.out.println("Trying liquiTradeService.getOrder(orderIDs)\n");
+
             Collection<Order> orders = liquiTradeService.getOrder("1");
+
+            System.out.println("Success! Result:");
+            System.out.println(orders.toArray().toString());
+            System.out.println("\n\n----------------------------------------------------------------------\n\n");
         } catch (NotAvailableFromExchangeException e) {
             System.err.println("NotAvailableFromExchangeException " + e.getMessage());
         } catch (NotYetImplementedForExchangeException e) {
